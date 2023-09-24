d3509ec5dfa08962a03a162a6013ae7341f9304d
Agusti Bayu <80197846+AgustiBayu@users.noreply.github.com>
2023-09-24 23:01:52 +0700

    1# basic design

---
+++ src/screens/Kalkulator.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Text, View } from "react-native"
+
+export default function Kalkulator() {
+    return(
+        <View style={{backgroundColor: 'white', flex: 1}}>
+            <Text>HELLO AGUSTI SAMUDRO</Text>
+        </View>
+    )
+}
